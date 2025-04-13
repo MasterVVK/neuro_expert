dc42f4f919f62dc54bf7cd2fb6652c2c7be5dcfd
MasterVVK <vvk@2140101.ru>
2025-04-13 11:56:10 +0000

    up

--- convert_pdf_docling.py
+++ convert_pdf_docling.py
@@ -14,7 +14,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
-
 # Проверяем наличие docling
 try:
     import docling
