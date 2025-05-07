581f5c399b6f8dcdab1b930ebdf8733beb26ec34
MasterVVK <vvk@2140101.ru>
2025-05-07 14:10:51 +0000

    MV ppee_analyzer

--- ppee_analyzer/checklist/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Модуль для работы с чек-листами и анализа соответствия документов
-"""
-
-from .analyzer import ChecklistAnalyzer
-
-__all__ = ['ChecklistAnalyzer']
--- ppee_analyzer/semantic_chunker/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Модуль для семантического разделения документов ППЭЭ
-с использованием библиотеки docling.
-"""
-
-from .chunker import SemanticChunker, SemanticDocumentSplitter
-
-__all__ = ['SemanticChunker', 'SemanticDocumentSplitter']
