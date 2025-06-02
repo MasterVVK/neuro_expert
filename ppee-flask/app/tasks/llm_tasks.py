0774af1e513fc507d15d6c3b641b9a007c56dee0
MasterVVK <vvk@2140101.ru>
2025-06-02 10:07:12 +0000

    max_attempts = 1200  # Максимум 20 минут

--- ppee-flask/app/tasks/llm_tasks.py
+++ ppee-flask/app/tasks/llm_tasks.py
@@ -60,7 +60,7 @@ def process_parameters_task(self, application_id):
 
             if response.status_code == 200:
                 # Опрашиваем статус анализа через FastAPI
-                max_attempts = 600  # Максимум 10 минут
+                max_attempts = 1200  # Максимум 20 минут
                 attempt = 0
 
                 while attempt < max_attempts:
